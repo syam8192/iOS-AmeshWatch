02f35358c5264bfe1631e57ba94fbfeb79317416
山上 忍 <yamakami_shinobu_gn@cyberagent.co.jp>
2018-01-22 11:22:20 +0900

    Initial Commit

---
+++ AmeshWatch/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AmeshWatch
+//
+//  Created by 山上 忍 on 2018/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
